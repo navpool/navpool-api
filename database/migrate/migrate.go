cf11bd48ccbad94d3251ae3960d4eb5b3381efee
dantudor <dtudor01@gmail.com>
2019-07-30 20:42:09 +0100

    delete migrate

--- database/migrate/migrate.go
+++
@@ -1,9 +0,0 @@
-package migrate
-
-import (
-	"github.com/NavPool/navpool-api/account"
-	"github.com/NavPool/navpool-api/database"
-	//"github.com/NavPool/navpool-hq-api/service/account"
-	//"github.com/NavPool/navpool-hq-api/service/address/model"
-	//model2 "github.com/NavPool/navpool-hq-api/service/communityFund/model"
-)
